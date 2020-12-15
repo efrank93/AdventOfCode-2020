c86b622b45476f70de7fa22d97bacf216d1780a6
Enrico Franchetti <e.franchetti93@gmail.com>
2020-12-15 22:19:05 +0100

    day15_1

--- days_src/day15.py
+++ days_src/day15.py
@@ -1,36 +1,44 @@
-def main():
-    #numbers = []
-    numbers = open('input_days/day15.txt', 'r').read().split(',')
-    #for val in f:
-       # numbers.append(int(val))
+import time
 
-    day15_1(numbers)
+def main():    
+    input = [0,3,1,6,7,5]
+    numbers_1 = {}
+    numbers_2 = {}
     
-def day15_1(numbers):
-    min = len(numbers)
-    lastIndex = 0
-    for i in range(min, 2020):        
-        if numbers.count(numbers[i-1]) == 1:
-            if '0' not in numbers:
-                numbers.append('0')
-            else: 
-                lastIndex = len(numbers)-numbers[::-1].index('0')-1
-                numbers.append('0')                    
-        elif numbers[i-1] == numbers[i-2]:
-            if '1' not in numbers:
-                numbers.append('1')
-            else: 
-                lastIndex = len(numbers)-numbers[::-1].index('1')-1
-                numbers.append('1')
+    for i in range(0, len(input)):
+        numbers_1[int(input[i])] = i
+        numbers_2[int(input[i])] = i
+        
+    start_time = time.time()
+    day15(numbers_1, 2020)
+    day15(numbers_2, 30000000)
+    
+    print("--- %s seconds ---" % (time.time() - start_time))
+    
+def day15(numbers, limit):
+    index = len(numbers)
+    value = 0
+    while index < limit:    
+        maxKeys = numbers.keys()
+        if not value in maxKeys:
+            numbers[value] = index
+            value = 0
+            index += 1
         else:
-            value = str(i-1 - lastIndex)
-            if not value in numbers:
-                numbers.append(value)
-            else:
-                lastIndex = len(numbers)-numbers[::-1].index(str(value))-1
-                numbers.append(value)
-            
-    print(numbers[2019])
+            lastIndex = index - numbers[value]
+            if lastIndex == 1:
+                numbers[value] = index
+                value = 1
+            else: 
+                numbers[value] = index
+                value = lastIndex
+                
+            index += 1
+
+        if index == limit:
+            for k, v in numbers.items():
+                if v == limit-1:
+                    print(k)
 
 if __name__ == "__main__":
     main()
