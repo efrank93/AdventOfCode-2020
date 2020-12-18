8d7a50fb0de6e7e654b6f7b80b049f574b512c2b
Enrico Franchetti <efranchette93@gmail.com>
2020-12-18 14:53:33 +0100

    day18

---
+++ days_src/day18.py
@@ -0,0 +1,12 @@
+def main():
+    with open('input_days/day18.txt', 'r') as input:
+        inputfile = [line.split() for line as input]
+        
+    day18_1(inputfile)
+    
+def day18_1(expressionList):
+    for expression in expressionList:
+        
+
+if __name__ == "__main__":
+    main()
